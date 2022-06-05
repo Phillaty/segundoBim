7b41208afab5a7e4a6b87bea9775d5bb8c3ff699
Gustavo <gustavovb33@gmail.com>
2022-06-04 23:07:39 -0300

    alt

---
+++ SysPatrimonio/Models/DtoDepartamento.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SysPatrimonio.Models
+{
+    public class DtoDepartamento
+    {
+        public int id { get; set; }
+        public string nomedepartamento { get; set; }
+        public string descricaodepartamento { get; set; }
+
+        public int idlocal { get; set; }
+        public string nomelocal { get; set; }
+    }
+}
